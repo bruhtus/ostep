0da0698eb0e5c5c1ffb06e2bff03f078646f632a
Robertus Chris <hibruhtus@gmail.com>
2026-02-20 05:52:55 +0700

    feat(chapter-5): add example of using waitpid() system call
    This is a simple example of using waitpid() to wait the child process
    to return before continuing the parent process.

---
+++ chapter-5/p2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/*
+ * Reference:
+ * https://pages.cs.wisc.edu/~remzi/OSTEP/cpu-api.pdf
+ */
+int main(void)
+{
+	printf("Start (PID: %d)\n", getpid());
+
+	int rc = fork();
+
+	if (rc < 0) {
+		fprintf(stderr, "fork failed\n");
+		return 1;
+	} else if (rc == 0) {
+		printf("Child (PID: %d)\n", getpid());
+		return 42;
+	} else {
+		int wstatus;
+
+		/*
+		 * We are waiting for the child process
+		 * to return first, before continuing
+		 * the next instruction.
+		 *
+		 * Keep in mind that waitpid() might
+		 * return _before_ the child process exit.
+		 * According to man page, this system call
+		 * will return immediately if the child
+		 * process __changed__ state, not
+		 * only when the child process finish but
+		 * also when the child process resumed by
+		 * a signal.
+		 */
+		int rc_wait = waitpid(rc, &wstatus, 0);
+
+		printf(
+			"Parent of %d (rc_wait: %d, PID: %d, wstatus: %d, child_exit_code: %d)\n",
+			rc,
+			rc_wait,
+			getpid(),
+			wstatus,
+			WEXITSTATUS(wstatus)
+		);
+	}
+
+	return 0;
+}
