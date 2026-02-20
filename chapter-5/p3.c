299ac94925add13cae9111e11d3a4719eb541b6c
Robertus Chris <hibruhtus@gmail.com>
2026-02-21 06:11:14 +0700

    feat(chapter-5): add example of using exec() system call
    Looks like exec() did not create new process like fork() does, but
    rather replacing the current process instructions with another
    executable instructions.
    
    Currently still not sure if that explanation is correct, need more info.

---
+++ chapter-5/p3.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/*
+ * Reference:
+ * https://pages.cs.wisc.edu/~remzi/OSTEP/cpu-api.pdf
+ */
+int main(void)
+{
+	printf("Start (PID: %d)\n", getpid());
+
+	int rc = fork();
+
+	if (rc < 0) {
+		fprintf(stderr, "fork failed\n");
+		return 1;
+	} else if (rc == 0) {
+		printf("Child (PID: %d)\n", getpid());
+
+		char *const my_args[3] = {
+			"wc",
+			__FILE__,
+			NULL
+		};
+
+		/*
+		 * When we use exec(), we are replacing
+		 * the program in the _current_ process
+		 * with another program. In this case,
+		 * we are replacing the child process
+		 * from fork() with `wc` program.
+		 *
+		 * So we are __not creating a new process__,
+		 * but we are __reusing__ the current
+		 * process with different instructions.
+		 *
+		 * That's why the next instruction
+		 * (or statement) after exec() is not
+		 * executed.
+		 */
+		int err = execvp(my_args[0], my_args);
+
+		/*
+		 * exec() will only return a value when
+		 * an error occured, such as when the
+		 * executable does not exist.
+		 */
+		if (err == -1) {
+			perror("exec() error");
+		}
+
+		printf("This should not be executed\n");
+	} else {
+		int wstatus;
+
+		int rc_wait = waitpid(rc, &wstatus, 0);
+
+		printf(
+			"Parent of %d (rc_wait: %d, PID: %d, wstatus: %d, child_exit_code: %d)\n",
+			rc,
+			rc_wait,
+			getpid(),
+			wstatus,
+			WEXITSTATUS(wstatus)
+		);
+	}
+
+	return 0;
+}
