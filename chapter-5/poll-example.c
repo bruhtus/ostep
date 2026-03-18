b39fe33b32e47e5c38a2b6c674dcfd41d7465f5c
Robertus Chris <hibruhtus@gmail.com>
2026-03-18 07:01:46 +0700

    feat(chapter-5): add example of poll()
    This first attempt trying to understand poll(). But there's still
    something that I am not sure.

---
+++ chapter-5/poll-example.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <poll.h>
+#include <unistd.h>
+
+#define ARRAY_LEN(array) (sizeof(array) / sizeof(*array))
+
+/*
+ * Reference:
+ * https://github.com/skuhl/sys-prog-examples/blob/master/simple-examples/poll.c
+ */
+int main(void)
+{
+	struct pollfd fds[] = {
+		{
+			.fd = STDIN_FILENO,
+			.events = POLLIN
+		}
+	};
+
+	/*
+	 * Timeout in miliseconds.
+	 *
+	 * Return a non-negative value, which is a number
+	 * of elements in the pollfd array whose `revents`
+	 * fields have been set to a non-zero value
+	 * (indicating an event or an error).
+	 */
+	int rc_poll = poll(fds, ARRAY_LEN(fds), 1000);
+
+	if (rc_poll == -1) {
+		perror("poll() error");
+		return 69;
+	} else if (rc_poll == 0) {
+		printf("poll() timed out\n");
+		return 0;
+	} else {
+		/*
+		 * `revents` is an output parameter filled
+		 * by the kernel with the events that
+		 * actually occurred.
+		 *
+		 * To trigger this condition, we can add
+		 * `close(STDIN_FILENO)` before calling
+		 * poll().
+		 */
+		if (fds[0].revents & POLLNVAL) {
+			printf("Bad file descriptor\n");
+			return 69;
+		}
+
+		printf("data available now!\n");
+	}
+
+	/*
+	 * Still not sure _why_ we need to do this but if
+	 * we didn't do this, the character we type will
+	 * be sent to the shell too.
+	 *
+	 * If we type more characters than the provided
+	 * buffer, we will sent the remaining character to
+	 * the shell too.
+	 */
+	if (1) {
+		char buf[2];
+		read(STDIN_FILENO, buf, ARRAY_LEN(buf));
+	}
+
+	return 0;
+}
