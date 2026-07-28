4118f0de418dfed69bc75322ed8e2f4031d53d33
Robertus Chris <hibruhtus@gmail.com>
2026-07-28 10:05:41 +0700

    feat(chapter-26): add simple thread creation example
    In this example, we try to create thread using pthread_create() and wait
    for those thread to terminate with pthread_join().
    
    There are some cases where the first thread created might not be the
    first one to run, but currently I am still not sure how to reproduce
    that.

---
+++ chapter-26/t0.c
@@ -0,0 +1,89 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+void *my_thread(void *arg);
+
+int main(void)
+{
+	pthread_t p1, p2;
+	const char *err;
+	int retval;
+
+	char *p1_arg = "A";
+	char *p2_arg = "B";
+
+	puts("main: begin");
+
+	retval = pthread_create(
+		&p1,
+		NULL,
+		my_thread,
+		p1_arg
+	);
+	if (retval != 0) {
+		err = strerror(errno);
+		printf(
+			"pthread_create() failed: %s (line %d)\n",
+			err,
+			__LINE__
+		);
+		return 69;
+	}
+
+	retval = pthread_create(
+		&p2,
+		NULL,
+		my_thread,
+		p2_arg
+	);
+	if (retval != 0) {
+		err = strerror(errno);
+		printf(
+			"pthread_create() failed: %s (line %d)\n",
+			err,
+			__LINE__
+		);
+		return 69;
+	}
+
+	/*
+	 * Wait the pthread to terminate.
+	 */
+	retval = pthread_join(p1, NULL);
+	if (retval != 0) {
+		err = strerror(errno);
+		printf(
+			"pthread_join() failed: %s (line %d)\n",
+			err,
+			__LINE__
+		);
+		return 69;
+	}
+
+	/*
+	 * TODO:
+	 * How to make sure that p2 run first?
+	 */
+	retval = pthread_join(p2, NULL);
+	if (retval != 0) {
+		err = strerror(errno);
+		printf(
+			"pthread_join() failed: %s (line %d)\n",
+			err,
+			__LINE__
+		);
+		return 69;
+	}
+
+	puts("main: end");
+
+	return 0;
+}
+
+void *my_thread(void *arg)
+{
+	printf("%s\n", (const char *)arg);
+	return NULL;
+}
